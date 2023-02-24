e170e3228cb44045beaf850418e175af24b7bc24
fcakiroglu16 <f-cakiroglu@outlook.com>
2023-02-24 11:09:03 +0300

    2.saat

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -22,12 +22,31 @@ export class AppComponent {
 
 
 
-     this.fakeService.UpdatePut({id:1, title:'title 1',body:'body 1', userId:2}).subscribe({
-      next:(data)=>console.log(data),
-      error:(err)=> console.log(err.message),
-      complete:()=>console.log("tamamalandı.")
+// this.fakeService.getParallelWithForkJoinOperator().subscribe(x=>console.log(x.posts))
+
+
+
+this.fakeService.getPostsWithUserId(1).subscribe(x=>{
+  console.log(x)
+})
+
+
+
+// this.fakeService.getParallelRequest().postObservable.subscribe(x=>console.log(x))
+
+// this.fakeService.getParallelRequest().userObservable.subscribe(x=>console.log(x))
+
+// this.fakeService.deletePost(1).subscribe(x=>{
+//   console.log(x);
+// })
+
+
+    //  this.fakeService.UpdatePut({id:1, title:'title 1',body:'body 1', userId:2}).subscribe({
+    //   next:(data)=>console.log(data),
+    //   error:(err)=> console.log(err.message),
+    //   complete:()=>console.log("tamamalandı.")
       
-      })
+    //   })
 
 
 // this.fakeService.SavePostWithError({id:1, title:'title 1',body:'body 1', userId:2}).subscribe({
