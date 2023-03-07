6cdf12c3323c44422926db0eef9696a585f239b6
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2023-03-07 10:40:41 +0300

    2.saat

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextboxComponent } from './textbox/textbox.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { TodoSaveComponent } from './pages/todo-save/todo-save.component';
+import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
 
 
 
@@ -13,7 +15,10 @@ import { TextboxComponent } from './textbox/textbox.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TextboxComponent,
+    TodoListComponent,
+    TodoSaveComponent,
+    TodoUpdateComponent
+
 
 
   ],
---
+++ app/src/app/pages/todo-save/todo-save.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-save',
+  templateUrl: './todo-save.component.html',
+  styleUrls: ['./todo-save.component.css']
+})
+export class TodoSaveComponent {
+
+}
